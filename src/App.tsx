9288f7664e34458bcaaabaa21d94f11856ad00cf
SeungrokYoon <sonicrok93@gmail.com>
2025-11-23 00:51:16 +0900

    env: tailwindCSS 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import CheckMyGroupPageForAllNation from "./pages/CheckMyGroupPageForAllNationPage";
-import CheckMyNewGroupPage from "./pages/CheckMyNewGroupPage";
-import MainPage from "./pages/MainPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import EventPage from "./pages/EventPage";
+import CheckMyGroupPageForAllNation from './pages/CheckMyGroupPageForAllNationPage';
+import CheckMyNewGroupPage from './pages/CheckMyNewGroupPage';
+import MainPage from './pages/MainPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import EventPage from './pages/EventPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <MainPage />,
   },
   {
-    path: "/all-nation-old-group",
+    path: '/all-nation-old-group',
     element: <CheckMyGroupPageForAllNation />,
   },
   {
-    path: "/new-group-check-my-group",
+    path: '/new-group-check-my-group',
     element: <CheckMyNewGroupPage />,
   },
 
   {
-    path: "/event",
+    path: '/event',
     element: <EventPage />,
   },
 ]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './styles/global.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './styles/global.css';
+import App from './App.tsx';
 
 // QueryClient 생성
 const queryClient = new QueryClient({
@@ -13,12 +13,12 @@ const queryClient = new QueryClient({
       staleTime: 1000 * 60 * 5, // 5분
     },
   },
-})
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
