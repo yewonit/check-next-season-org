3dfc279f0385eb1ed0d500ba3792d86e47aa3c70
SeungrokYoon <sonicrok93@gmail.com>
2025-12-03 22:45:23 +0900

    env: React Query Devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles/global.css';
 import App from './App.tsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // QueryClient 생성
 const queryClient = new QueryClient({
@@ -32,6 +33,7 @@ enableMocking().then(() => {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
     </StrictMode>
   );
